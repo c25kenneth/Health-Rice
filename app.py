f7043aa05b6c32f4ba21a468d668ac83f45aa11f
thequickbrownfoxjumpedoverthelazydog <c25kenneth@gmail.com>
2022-02-06 08:47:42 -0800

    Renamed testing images and got twilio api working

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from email import message
 from lib2to3.pgen2 import token
 import pandas as pd
 import numpy as np
@@ -8,16 +9,23 @@ import cv2
 from tensorflow import keras
 from img_classification import machine_classification
 import tensorflow as tf
+from twilio.rest import Client
 
+client = Client('**********************', '**************************')
 
 physical_devices = tf.config.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
-def predictImage():
-    pass
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
 st.sidebar.title('About HealthRice')
-st.sidebar.info('''Rice is the most popular staple in the world. It is a crucial staple for almost 
+st.sidebar.info('''Rice is the most popular staple in the world. It is a crucial food for almost 
 4 billion people worldwide. Problem is, these crops can experience a variety of diseases. 
 These diseases can cause sickness to humans when eaten. While these disease are quite hard to diagnose, HealthRice
 is here to help! HealthRice utilizes the power of machine learning in order to diagnose BrownSpot and Hispa. Of course,
@@ -53,7 +61,28 @@ if uploaded_file is not None:
       label = machine_classification(image, 'keras_model.h5')
       if label == 0:
           st.write("The inputted rice leaf appears to have Brownspot. It is not recommendeed to sell this to the public.")
+          if (phoneNumber is not None):
+            message = client.messages.create(
+                body='Your leaf appears to have BrowSpot!',
+                from_ = '+19362463603', 
+                to=phoneNumber
+            )
+            print(message.body)
       elif label == 1:
           st.write("Congratulations! The inputted rice leaf is healthy!")
+          if (phoneNumber is not None):
+            message = client.messages.create(
+                body='Congrats! Your leaf is healthy!',
+                from_ = '+19362463603', 
+                to=phoneNumber
+            )
+            print(message.body)
       else: 
         st.write('The inputted rice leaf unfortunately has Hispa. It is not recommended to sell this to the public. ')
+        if phoneNumber is not None:
+            message = client.messages.create(
+                body = 'The inputted rice leaf unfortunately appears to have Hispa', 
+                from_ = '+19362463603',
+                to = phoneNumber
+            )
+            print(message.body)
